957a1233a136abb7f50019e5d835afba8533132c
Benjamin Sypher <ben.sypher@gmail.com>
2018-04-01 19:20:00 -0400

    12:uses ERB template and HTML

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,10 +1,10 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-x= (rand(100))
+number= (rand(100))
 
 get '/' do 
+	erb :index, :locals => {:number => number}
 	
-	
-"The SECRET NUMBER is " + x.to_s
+
 end
